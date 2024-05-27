d8702f492a7e7f8ce980fd6980aee72046467ab4
fuzoh <info@bastiennicoud.ch>
2024-05-27 20:52:23 +0200

    feat: class to represent api results

---
+++ src/main/java/ch/hearc/ig/zip_favre_casa/entities/Field.java
@@ -0,0 +1,10 @@
+package ch.hearc.ig.zip_favre_casa.entities;
+
+public class Field {
+    String Title;
+    String Code;
+    String Value;
+    boolean IsRequired;
+    boolean IsReadOnly;
+    boolean IsVisible;
+}
---
+++ src/main/java/ch/hearc/ig/zip_favre_casa/entities/Object.java
@@ -0,0 +1,15 @@
+package ch.hearc.ig.zip_favre_casa.entities;
+
+import java.util.List;
+
+public class Object {
+    int ObjectID;
+    int ContentTypeID;
+    boolean IsInWorkflow;
+    List<Field> Fields;
+    String ContentType;
+    String Version;
+    String Author;
+    String CreationDate;
+    String Extension;
+}
