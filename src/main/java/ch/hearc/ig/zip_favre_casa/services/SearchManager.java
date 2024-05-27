365c756be77d122186a1b7634f6c154534c3ec0a
fuzoh <info@bastiennicoud.ch>
2024-05-27 20:57:05 +0200

    feat: search now returns a string with the result

--- src/main/java/ch/hearc/ig/zip_favre_casa/services/SearchManager.java
+++ src/main/java/ch/hearc/ig/zip_favre_casa/services/SearchManager.java
@@ -3,20 +3,21 @@ package ch.hearc.ig.zip_favre_casa.services;
 import ch.hearc.ged.JSONUtilities;
 
 import java.net.HttpURLConnection;
+import java.util.Optional;
 
 public class SearchManager {
 
-    public void search(String token) {
+    public String search(String token) throws Exception {
         String url = "http://157.26.83.80:2240/api/search/advanced";
         String requestBody = "{searchPattern: \"_etat|l01|Accepter - En attente de paiement|list\", contentTypeIDs: \"137\"}";
+        // Ajout de l'en-tête d'autorisation avec le jeton
+        HttpURLConnection connection = JSONUtilities.write(
+                url,
+                JSONUtilities.RequestMethod.POST,
+                requestBody,
+                token
+        );
+        return JSONUtilities.read(connection);
 
-        try {
-            // Ajout de l'en-tête d'autorisation avec le jeton
-            HttpURLConnection connection = JSONUtilities.write(url, JSONUtilities.RequestMethod.POST, requestBody, token);
-            String response = JSONUtilities.read(connection);
-            System.out.println("Response: " + response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
